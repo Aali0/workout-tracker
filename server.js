5ee43ddca4dda814211feb257014239bd0ae2602
Ahmed Ali <ahmed.adil.ali@gmail.com>
2021-03-13 16:12:28 -0800

    server edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 const app = express();
 
